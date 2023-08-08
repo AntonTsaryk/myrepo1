44aa570b196847f27231635a40d8d51f85f62077
Roza <122153166+Vlada2890@users.noreply.github.com>
2023-08-08 16:16:12 +0200

    Update views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
 def login_view(request):
     if request.method == 'POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        user =authenticate(request,username=username,password=password)
-        if user is not None:    
-            login(request,user)
-            return redirect ('home')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') 
         else:
-            return render(request, 'authentication/home.html', {'error': 'Bad credentials'})
-    else: 
+            return render(request, 'home.html', {'error': 'Bad credentials'})
+    else:
         return render(request, 'authentication/login.html')
-    
+
+
 def logout_view(request):
     logout(request)
     return redirect('login')
-
-def home_view(TemplateView):
-    template_name = 'home.html'
